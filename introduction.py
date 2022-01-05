822933d834e1340b9aa5c0596c1ca014642a2c2c
aashisharma22 <96761521+aashisharma22@users.noreply.github.com>
2022-01-05 16:21:42 +0530

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,5 @@
+def intro():
+    print('Hey Guys. My name is Aashish Sharma. I turned 18 last year and I am currently studying Computer Science Engineering.')
+    print()
+    print('I have studied Python and a little bit of HTML. I am currently learning C++.')
+intro()   
